49012cd6cf5fd6e8bc2512c60f8290cbcedf7439
Matheus Felipe <theusf40@hotmail.com>
2026-02-22 20:21:18 -0300

    Add teste LTI

---
+++ index.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+// Require Provider 
+const lti = require('ltijs').Provider
+
+// Setup provider
+lti.setup('LTIKEY', // Key used to sign cookies and tokens
+  { // Database configuration
+    url: 'mongodb://localhost/database',
+    connection: { user: 'user', pass: 'password' }
+  },
+  { // Options
+    appRoute: '/', loginRoute: '/login', // Optionally, specify some of the reserved routes
+    cookies: {
+      secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
+      sameSite: '' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+    },
+    devMode: true // Set DevMode to false if running in a production environment with https
+  }
+)
+
+// Set lti launch callback
+lti.onConnect((token, req, res) => {
+  console.log(token)
+  return res.send('It\'s alive!')
+})
+
+const setup = async () => {
+  // Deploy server and open connection to the database
+  await lti.deploy({ port: 3000 }) // Specifying port. Defaults to 3000
+
+  // Register platform
+  await lti.registerPlatform({
+    url: 'https://platform.url',
+    name: 'Platform Name',
+    clientId: 'TOOLCLIENTID',
+    authenticationEndpoint: 'https://platform.url/auth',
+    accesstokenEndpoint: 'https://platform.url/token',
+    authConfig: { method: 'JWK_SET', key: 'https://platform.url/keyset' }
+  })
+}
+
+setup()
